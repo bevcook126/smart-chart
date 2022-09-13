38c1c958a9b60d3e18f60f11f56f08c4409ba172
username <bevcook126@gmail.com>
2022-09-13 10:32:26 -0700

    indentation

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NavBar from '../../components/NavBar/NavBar';
