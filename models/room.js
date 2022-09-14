33a630f4db1b47c8e996d6853621847ebe5f468b
username <bevcook126@gmail.com>
2022-09-14 08:00:18 -0700

    update

--- models/room.js
+++ models/room.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const deskSchema = require('./desk');
 
 const roomSchema = new Schema({
-  // desk : deskSchema,
   user: {type:Schema.Types.ObjectId, ref: 'User'},
   layout: { type: Array, required: true }
 }, {
